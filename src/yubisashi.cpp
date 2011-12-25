75ddde831bc6c3ebcbfec9f90fb7df555309e863
Truong Tuan Manh <manhattan.vn@gmail.com>
2011-12-25 21:28:10 +0900

    Changed

--- src/yubisashi.cpp
+++ src/yubisashi.cpp
@@ -44,8 +44,12 @@
 #include <errno.h>
 
 #include <cv_bridge/cv_bridge.h>
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+//#include <opencv/cv.h>
+//#include <opencv/highgui.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 
 #include <pthread.h>
 
